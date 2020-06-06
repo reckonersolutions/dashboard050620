5de50c283d725236be8011ae46cb92cd744b32b2
Haseeb <haseeb.patron@yahoo.com>
2020-06-06 22:14:27 +0400

    06062214

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { AreaComponent } from './widgets/area/area.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    AreaComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatListModule,
+    HighchartsChartModule
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    AreaComponent
+  ]
+})
+export class SharedModule { }
