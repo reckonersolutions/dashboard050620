8a9d80c44bb2d048c60587e169a08a1dd079a5df
Haseeb <haseeb.patron@yahoo.com>
2020-06-07 00:17:22 +0400

    07060017

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -7,6 +7,12 @@ import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule} from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule} from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DashboardService } from 'src/app/modules/dashboard.service';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +25,14 @@ import { MatDividerModule} from '@angular/material/divider';
     RouterModule,
     SharedModule,
     MatSidenavModule,
-    MatDividerModule
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule
+  ],
+  providers: [
+    DashboardService
   ]
 })
 export class DefaultModule { }
